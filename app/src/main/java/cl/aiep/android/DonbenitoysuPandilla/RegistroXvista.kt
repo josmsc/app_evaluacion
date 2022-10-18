210e3ee246ca37f3610f9f3a324e95d5a198f015
Jose Moscoso <jose.f.moscoso@hotmail.com>
2022-10-17 22:31:16 -0300

    recyrclerview

--- app/src/main/java/cl/aiep/android/DonbenitoysuPandilla/RegistroXvista.kt
+++ app/src/main/java/cl/aiep/android/DonbenitoysuPandilla/RegistroXvista.kt
@@ -5,7 +5,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import androidx.recyclerview.widget.LinearLayoutManager
+import cl.aiep.android.DonbenitoysuPandilla.Constantes.KEY_DESCRIPCION
+import cl.aiep.android.DonbenitoysuPandilla.Constantes.KEY_ESPECIALIDAD
+import cl.aiep.android.DonbenitoysuPandilla.Constantes.KEY_LOCALIDAD
+import cl.aiep.android.DonbenitoysuPandilla.Constantes.KEY_NOMBRE
+import cl.aiep.android.DonbenitoysuPandilla.Constantes.TABLA_LUGARES
+import cl.aiep.android.DonbenitoysuPandilla.Lugares.Lugares
+import cl.aiep.android.DonbenitoysuPandilla.adaptador.LugaresAdapter
 import cl.aiep.android.DonbenitoysuPandilla.databinding.ActivityRegistroXVistaBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import org.checkerframework.checker.index.qual.GTENegativeOne
 
 class RegistroXvista : AppCompatActivity() {
 
@@ -28,6 +42,53 @@ class RegistroXvista : AppCompatActivity() {
     private fun ingresar() {
         startActivity(Intent(this,Formulario::class.java))
     }
+
+    //Funcion para listar la data
+    fun listarData(fuenteDatos:String){
+        val listaData = ArrayList<Lugares>()
+        val db = Firebase.firestore
+        //que tabla traeremos
+        db.collection(TABLA_LUGARES)
+            .orderBy("nombre")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result){
+                    Log.d("Data", "${document.id} => ${document.data}")
+                    //Se crea un objeto lugar y se crea como objeto para agregarlo a la vista
+                    val lugar = Lugares(
+                        document.id,
+                        document.data[KEY_NOMBRE].toString(),
+                        document.data[KEY_LOCALIDAD].toString(),
+                        document.data[KEY_ESPECIALIDAD].toString(),
+                        document.data[KEY_DESCRIPCION].toString()
+                    )
+                    //Se agrega la data a la lista
+                    listaData.add(lugar)
+                }
+                //Configurar la lista
+                mostrarDataEnLista(listaData)
+            }
+            .addOnFailureListener { exception ->
+                binding.listaData.visibility = GONE
+                binding.progressBar.visibility = GONE
+                Log.w("Data", "Error getting documents.", exception)
+            }
+
+    }
+
+    private fun mostrarDataEnLista(listaData:ArrayList<Lugares>){
+        val adaptadorDataLugares = LugaresAdapter(listaData, binding, this)
+        //configurar la lista del recyclerview
+        binding.listaData.apply {
+            layoutManager = LinearLayoutManager (applicationContext)
+            adapter = adaptadorDataLugares
+        }
+        //Mostrar la lista y ocultar el lugar
+        binding.progressBar.visibility = GONE
+        binding.listaData.visibility = VISIBLE
+    }
+
+
 
 
 }
---
+++ app/src/main/java/cl/aiep/android/DonbenitoysuPandilla/adaptador/LugaresAdapter.kt
@@ -0,0 +1,53 @@
+package cl.aiep.android.DonbenitoysuPandilla.adaptador
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import cl.aiep.android.DonbenitoysuPandilla.Lugares.Lugares
+import cl.aiep.android.DonbenitoysuPandilla.R
+import cl.aiep.android.DonbenitoysuPandilla.RegistroXvista
+import cl.aiep.android.DonbenitoysuPandilla.databinding.ActivityRegistroXVistaBinding
+
+class LugaresAdapter (
+    val listaLugar: List<Lugares>,
+    val binding: ActivityRegistroXVistaBinding,
+    val listarLugar: RegistroXvista
+    ):
+        RecyclerView.Adapter<LugaresAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val txtNombre: TextView
+        val txtLocalidad: TextView
+        val txtEspecialidad: TextView
+        val txtDescripcion: TextView
+        val btnReserva: Button
+
+        init {
+            txtNombre = view.findViewById(R.id.txtNombreLocal)
+            txtLocalidad = view.findViewById(R.id.txtLocalidad)
+            txtEspecialidad = view.findViewById(R.id.txtEspecialidad)
+            txtDescripcion = view.findViewById(R.id.txtdescripcion)
+            btnReserva = view.findViewById(R.id.btnReservar)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_lista, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.txtNombre.text = listaLugar.get(position).nombre
+        holder.txtLocalidad.text = listaLugar.get(position).localidad
+        holder.txtEspecialidad.text = listaLugar.get(position).especialidad
+        holder.txtDescripcion.text = listaLugar.get(position).descripcion
+        }
+
+    override fun getItemCount(): Int {
+        return listaLugar.size
+    }
+}
+
